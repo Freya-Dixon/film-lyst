f6069d61dd99ebca8bec3eb769c26e54033dc9bb
Freya-Dixon <freya.dixon@hotmail.com>
2021-08-24 15:23:01 +0100

    set up dummy database to render films on page

---
+++ src/assets/filmData.js
@@ -0,0 +1,59 @@
+
+const filmData =  [
+    { "name": "Interstellar (2014)" ,
+       "genre": "Sci-Fi",
+       "img": "https://miro.medium.com/max/1400/1*9UtTHaYSZrVtIS8WVzZJog.jpeg",
+       "streaming": "Amazon Prime",
+       "runTime": "2h 55m"
+ },
+ { "name": "Melancholia (2011)" ,
+ "genre": "Drama",
+ "img": "https://entertainment.time.com/wp-content/uploads/sites/3/2011/11/melancholia.jpg?w=720&h=480&crop=1",
+ "streaming": "Amazon Prime",
+ "runTime": "2h 16m"
+},
+
+{ "name": "The Life Aquatic" ,
+"genre": "Comedy",
+"img": "https://theartsdesk.com/sites/default/files/mastimages/Life_Aquatic_2.jpg",
+"streaming": "Amazon Prime",
+"runTime": "1h 59m"
+},
+
+{ "name": "Solaris" ,
+"genre": "Sci-Fi",
+"img": "https://static.rogerebert.com/uploads/review/primary_image/reviews/great-movie-solaris-1972/EB20030119REVIEWS08301190301AR.jpg",
+"streaming": "Mubi",
+"runTime": "2h 55m"
+},
+{ "name": "2001: A Space Odyssey (1968)" ,
+"genre": "Sci-Fi",
+"img": "https://static01.nyt.com/images/2018/05/15/science/15SCI-OUTTHERE1/15SCI-OUTTHERE1-videoSixteenByNineJumbo1600.jpg",
+"streaming": "BFI Player",
+"runTime": "2h 44m"
+ 
+},
+{ "name": "The Young Girls of Rochefort" ,
+"genre": "Musical ",
+"img": "https://fiaf.org/wp-content/uploads/2019/10/2020-10-cs-rochefort-m1.jpg",
+"streaming": "BFI Player",
+"runTime": "2h 08m"
+ 
+},
+{ "name": "Spirited Away" ,
+"genre": "Animation ",
+"img": "https://filmfork-cdn.s3.amazonaws.com/content/spiritedawaytracks.jpg",
+"streaming": "Netflix",
+"runTime": "2h 08m"
+ 
+},
+{ "name": "Goodfellas" ,
+"genre": "Comedy/Drama",
+"img": "https://nypost.com/wp-content/uploads/sites/2/2020/09/goodfellas-5.jpg?quality=80&strip=all",
+"streaming": "Mubi",
+"runTime": "2h 28m"
+ 
+},
+ ]
+     
+ export default filmData;
