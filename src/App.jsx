c4e381a59c3d5fac422a6ce27171fbc3962fc5a0
Freya-Dixon <freya.dixon@hotmail.com>
2021-08-25 12:19:09 +0100

    added routing system to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import styles from './App.module.scss'
 import UserDashboard from './Containers/UserDashboard'
 
 function App() {
   return (
     <>
+    <Router>
+    <Route exact path="/">
     <UserDashboard />
+    </Route>
+    </Router>
     </>
   );
 }
 
 export default App;
+
+
+
