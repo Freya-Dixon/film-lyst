3a49c6adfea82219533e4cf4339c6a367d225821
Freya-Dixon <freya.dixon@hotmail.com>
2021-08-25 09:22:03 +0100

    rendered components in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import styles from './App.module.scss'
+import UserDashboard from './Containers/UserDashboard'
+
+function App() {
+  return (
+    <>
+    <UserDashboard />
+    </>
+  );
+}
+
+export default App;
