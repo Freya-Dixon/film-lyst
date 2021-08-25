0433de00fefc198e27800e9d7d3a0b78f6bfb03e
Freya-Dixon <freya.dixon@hotmail.com>
2021-08-25 12:29:16 +0100

    added Nav bar to TV List

--- src/Components/TVList/TVList.jsx
+++ src/Components/TVList/TVList.jsx
@@ -2,22 +2,21 @@ import React from 'react'
 import FilmCard from '../../Components/FilmList/FilmCard'
 import styles from './TVList.module.scss'
 import filmData from '../../assets/filmData'
+import NavBar from '../NavBar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 
 const TVList = () => {
     return (
+        <>
+        <NavBar />
         <div className={styles.filmListWrapper}>
-        <h2 className={styles.subheading}>My Film Lyst <FontAwesomeIcon className={styles.icon} icon={faPlusSquare}></FontAwesomeIcon></h2> 
+        <h2 className={styles.subheading}>My TV Lyst <FontAwesomeIcon className={styles.icon} icon={faPlusSquare}></FontAwesomeIcon></h2> 
             <ul className={styles.filmListUl}> 
-            <FilmCard filmData={filmData[0]}/>  
-            <FilmCard filmData={filmData[2]}/>    
-            <FilmCard filmData={filmData[3]}/>  
-            <FilmCard filmData={filmData[5]}/>   
-            <FilmCard filmData={filmData[6]}/>   
-            <FilmCard filmData={filmData[7]}/>        
+            <FilmCard filmData={filmData[0]}/>        
             </ul>
         </div>
+        </>
     )
 }
 
