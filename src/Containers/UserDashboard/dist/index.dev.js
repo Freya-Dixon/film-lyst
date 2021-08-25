f592871100c18a469a64a3746a53fe652d39c379
Freya-Dixon <freya.dixon@hotmail.com>
2021-08-25 12:15:27 +0100

    added styling for app and side container, default background set

---
+++ src/Containers/UserDashboard/dist/index.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _UserDashboard = _interopRequireDefault(require("./UserDashboard"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = _UserDashboard["default"];
+exports["default"] = _default;
